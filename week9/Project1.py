408e99c5bab7f8df2a2fa0e08ece2ea430d7c9b8
Uchechukwu Nwafor <uchechukwu.nwafor@pau.edu.ng>
2025-05-14 03:16:48 -0700

    (My commit)

---
+++ week9/Project1.py
@@ -0,0 +1,36 @@
+import random
+
+class Employee:
+    def __init__(self,name):
+        self.name = name
+        self.employees = [
+            "Mary Evans", "Eyo Ishan", "Durojaiye Dare", "Adams Ali", "Andrew Ugwu",
+            "Stella Mankinde", "Jane Akibo", "Ago James", "Michell Taiwo", "Abraham Jones",
+            "Nicole Anide", "Kosi Korso", "Adele Martins", "Emmanuel Ojo", "Ajayi Fatima"
+        ]
+        self.tasks = ["Loading", "Transporting", "Reviewing Orders", "Customer Service", "Delivering Items"]
+        self.attendance = []
+
+    def check_employee(self):
+        return self.name in self.employees
+
+    def take_attendance(self):
+        if self.name not in self.attendance:
+            self.attendance.append(self.name)
+            return f"Attendance taken for {self.name}."
+        else:
+            return f"Attendance already taken for {self.name}."
+
+    def assign_task(self):
+        if self.check_employee():
+            return random.choice(self.tasks)
+        else: 
+            return self.refuse_access()
+
+    def refuse_access(self):
+        return "Access Denied. You are not an employee."
+    
+
+name = Employee(input("Enter your name: ") ) 
+
+print(name.employees())
